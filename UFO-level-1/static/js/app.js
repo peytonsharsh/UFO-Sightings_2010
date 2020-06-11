b10a8ed2c6630e9903a4083c5aec7341a6c3a8af
My Dude <peytonsharsh@gmail.com>
2020-06-11 16:03:59 -0500

    update 6.11

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,35 @@
+// from data.js
+var tableData = data;
+var tbody = d3.select("tbody");
+
+var filterbutton = d3.select("#submitbutton");
+
+//need to change to consider all filters
+var filterenter = d3.select("#form-control");
+
+filterbutton.on("click", runValue);
+filterenter.on("submit", runValue);
+ 
+function runValue() {
+    tbody.html("");
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datebox");
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+    d1 = Date.parse(inputValue)
+    data.forEach(function(sighting) {
+        d2 = Date.parse(sighting.datetime)
+        if (d1 === d2) {
+            var row = tbody.append("tr");
+            Object.entries(sighting).forEach(function([key, value]) {
+                console.log(key, value);
+                var cell = row.append("td");
+                cell.text(value);
+        })}
+    })
+};
+
+
+//inputField.on("change", function() {
+//   var newText = d3.event.target.value;
+//    console.log(newText);
